1908e9c24870c38822410d3136f6d58c686e3eca
Romain <rhennequin@deezer.com>
2017-04-01 12:05:18 +0200

    Fixed variable names: FREEZE->MRFREEZE

--- mod-mrfreeze.mk
+++ mod-mrfreeze.mk
@@ -1,22 +1,22 @@
 ######################################
 #
-# mod-freeze
+# mod-mrfreeze
 #
 ######################################
 
-MOD_FREEZE_VERSION = c2cbef39429b11c3c6fd3aab62c4a647ed80b895
-MOD_FREEZE_SITE = $(call github,romi1502,MrFreeze,$(MOD_FREEZE_VERSION))
-MOD_FREEZE_DEPENDENCIES = armadillo fftwf host-fftwf host-python host-python-mpmath
-MOD_FREEZE_BUNDLES = mod-mrfreeze.lv2
+MOD_MRFREEZE_VERSION = c2cbef39429b11c3c6fd3aab62c4a647ed80b895
+MOD_MRFREEZE_SITE = $(call github,romi1502,MrFreeze,$(MOD_MRFREEZE_VERSION))
+MOD_MRFREEZE_DEPENDENCIES = armadillo fftwf host-fftwf host-python host-python-mpmath
+MOD_MRFREEZE_BUNDLES = mod-mrfreeze.lv2
 
-MOD_FREEZE_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
+MOD_MRFREEZE_TARGET_MAKE = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(MAKE) NOOPT=true -C $(@D)
 
-define MOD_FREEZE_BUILD_CMDS
-	$(MOD_FREEZE_TARGET_MAKE)
+define MOD_MRFREEZE_BUILD_CMDS
+	$(MOD_MRFREEZE_TARGET_MAKE)
 endef
 
-define MOD_FREEZE_INSTALL_TARGET_CMDS
-	$(MOD_FREEZE_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) INSTALL_PATH=/usr/lib/lv2
+define MOD_MRFREEZE_INSTALL_TARGET_CMDS
+	$(MOD_MRFREEZE_TARGET_MAKE) install DESTDIR=$(TARGET_DIR) INSTALL_PATH=/usr/lib/lv2
 	cp -rL $($(PKG)_PKGDIR)/mod-mrfreeze.lv2/*   $(TARGET_DIR)/usr/lib/lv2/mod-mrfreeze.lv2/
 endef
 
